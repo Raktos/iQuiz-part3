59c182521fc1925e63ffa5f6208bb6971fd05150
Jason Ho <raktos@uw.edu>
2015-11-03 17:01:24 -0800

    maybe works idk can't build it

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    private let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    private let descriptions = ["Maths quizzes on maths.", "Superhero quizzes on Marvel.", "Science quizzes on science."]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier) as UITableViewCell?
+        
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        //let image = UIImage(named: "star")
+        //cell!.imageView?.image = image
+        
+        cell!.textLabel?.text = subjects[indexPath.row]
+        cell!.detailTextLabel?.text = descriptions[indexPath.row]
+        cell!.imageView?.image = UIImage(named: /*subjects[indexPath.row]*/"image.jpg")
+        return cell!
+    }
 
+    @IBAction func settingsPress(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Settings", message: "SETTINGS GO HERE", preferredStyle: UIAlertControllerStyle.Alert)
+        let alertAction = UIAlertAction(title: "OK!", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in }
+        alert.addAction(alertAction)
+        presentViewController(alert, animated: true) { () -> Void in }
+    }
 
 }
 
